cb47da9a483278e43d1f5048f4e8363c858958ac
noemieleitao <noemie.leitao@hotmail.fr>
2016-12-08 11:33:09 +0100

    first commit

---
+++ gotocelldialog.h
@@ -0,0 +1,17 @@
+#ifndef GOTOCELLDIALOG_H
+#define GOTOCELLDIALOG_H
+#include <QDialog>
+namespace Ui{
+    class GoToCellDialog ;
+}
+class GoToCellDialog : public QDialog{
+    Q_OBJECT
+    private:
+        Ui::GoToCellDialog *ui;
+    public :
+        explicit GoToCellDialog (QWidget *parent = 0);
+        ~GoToCellDialog();
+    private slots:
+    void on_lineEdit_textChanged();
+} ;
+#endif // GOTOCELLDIALOG H
